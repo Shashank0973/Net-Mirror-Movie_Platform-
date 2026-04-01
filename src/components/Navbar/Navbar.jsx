81258b977f08ab89d9dddfae9774a9e28b8bee48
Shashank Gupta <113624172+Shashank0973@users.noreply.github.com>
2026-04-02 02:17:09 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Navbar.css'
-import logo from '../../assets/logo.png' 
+import logo from '../../assets/Logo.png' 
 import search_icon from '../../assets/search_icon.svg'
 import bell_icon from '../../assets/bell_icon.svg'
 import profile_img from '../../assets/profile_img.png'
